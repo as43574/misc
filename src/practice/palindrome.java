43883706d5c2989b1aa6146547fa4c097c9fbae2
as43574 <adstorey@gmail.com>
2017-07-10 20:17:32 -0500

    Adds palindrome implementation and its tests

---
+++ src/practice/palindrome.java
@@ -0,0 +1,70 @@
+package practice;
+/*
+ * Checks if input string is a permutation of a palindrome
+ * 
+ */
+public class palindrome {
+	
+	public static boolean isPalendrome(String str) {
+		str = str.replaceAll(" ", "");
+		char[] ch = str.toCharArray();
+		java.util.Arrays.sort(ch);
+		boolean result = false;
+		if(ch.length %2 == 0) {
+			result = isEvenPalendrome(ch);
+		}else {
+			result = isOddPalendrome(ch);
+		}
+		
+		return result;
+	}
+	
+	// checks if there are an even number of each character, if length of str is even
+	public static boolean isEvenPalendrome(char[] ch) {
+		if(ch.length==0)
+			return true;
+		
+		int tmp = 1;
+		char c = ch[0];
+		for(int i=1; i<ch.length; i++) {
+			char prev = c;
+			c = ch[i];
+			if(prev == c) {
+				tmp++;
+			}else {
+				if(tmp%2 == 1)
+					return false;
+				tmp=1;
+			}
+			
+		}
+		return true;
+	}
+	
+	// checks if there are an even number of each character and one of one char,
+	// if length of str is odd
+		public static boolean isOddPalendrome(char[] ch) {
+			if(ch.length==1)
+				return true;
+			
+			int tmp = 1;
+			int charsAlone = 0;
+			char c = ch[0];
+			for(int i=1; i<ch.length; i++) {
+				char prev = c;
+				c = ch[i];
+				if(prev == c) {
+					tmp++;
+				}else {
+					if(tmp%2 == 1) {
+						charsAlone++;
+						if(charsAlone>1)
+							return false;
+					}
+					tmp=1;
+				}
+				
+			}
+			return true;
+		}
+}
---
+++ src/practice/palindromeTest.java
@@ -0,0 +1,26 @@
+package practice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class palindromeTest {
+
+	@Test
+	public void testIsPalendrome() {
+		assertTrue(palindrome.isPalendrome("abba"));
+		assertTrue(palindrome.isPalendrome("absba"));
+		assertTrue(palindrome.isPalendrome("ab ba"));
+		assertTrue(palindrome.isPalendrome("bbb"));
+		assertTrue(palindrome.isPalendrome("33332"));
+		assertTrue(palindrome.isPalendrome(""));
+
+
+		assertFalse(palindrome.isPalendrome("abbf"));
+		assertFalse(palindrome.isPalendrome("abc"));
+		assertFalse(palindrome.isPalendrome("12 3"));
+		assertFalse(palindrome.isPalendrome("a b"));
+
+	}
+
+}
